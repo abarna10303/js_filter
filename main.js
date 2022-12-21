296f90628a6db9dd04090be893800428427a6ddf
abarna10303 <abarnas361@gmail.com>
2022-12-21 10:11:13 +0530

    Filter

---
+++ main.js
@@ -0,0 +1,6 @@
+const values=[headphone=500,mouse=400,datacable=100,keyboard=900,charger=1000];
+let num=values.filter(function(value){
+    return value>500;
+});
+document.write(num);
+document.write(arname);
